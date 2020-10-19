faced79d093eb1e1bdd535faf51db2df440f07ce
Lucas Sousa <lucas103101@gmail.com>
2020-10-19 13:32:57 -0300

    Tabela com os campos necessário para nserir o dado

---
+++ databases/tablePostgres.sql
@@ -0,0 +1,18 @@
+﻿-- Table: public.workload
+
+-- DROP TABLE public.workload;
+
+CREATE TABLE public.workload
+(
+  id serial NOT NULL,
+  dado bytea,
+  CONSTRAINT workload_pkey PRIMARY KEY (id)
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE public.workload
+  OWNER TO lucas;
+
+--Comando para criar a tabela
+--psql -h localhost -U seuUser -d Banco < tablePostgres.sql
