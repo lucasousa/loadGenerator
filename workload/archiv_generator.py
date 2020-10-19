310388304200013898ee6e6df7d0f0dff50043ea
Jejinketsu <jederilson@gmail.com>
2020-10-19 20:43:16 -0300

    Modificando o gerador de workload para receber uma lista de dicionários com as informações de cad arquivo que deve ser gerado. Posteriormente isso pode ser colocado em um json com todos os testes.

--- workload/archiv_generator.py
+++ workload/archiv_generator.py
@@ -1,14 +1,14 @@
-def create_archiv(size_in_MB, name):
-    content = b''
+def create_archiv(size_in_MB, name, text):
+    content = b'' + bytes(text, 'utf-8')
 
-    content += (b'b' * (1024*1024*size_in_MB))
+    content += (content * (1024*1024*size_in_MB))
 
-    f = open(f'workload_{name}.txt','wb')
+    f = open(f'{name}.txt','wb')
     f.write(content)
     f.close
 
-def config_workload(size_in_MB, N_archivs):
-    for i in range(N_archivs):
-        create_archiv(size_in_MB, i)
+def config_workload(config_tests):
+    for test in config_tests:
+        create_archiv(test['text'], test['name'], test['char_content'])
 
-config_workload(1, 2)
+config_workload([{'text': 4, 'name': 'workload_4MB', 'char_content': 'c'}, {'text': 10, 'name': 'workload_10MB', 'char_content': 'd'}])
