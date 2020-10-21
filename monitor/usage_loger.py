f9a5d24a4cf83769bcadfff849b618d5128c6a92
Lucas Sousa <lucas103101@gmail.com>
2020-10-20 22:14:42 -0300

    Utilizando Thread para analisar o consumo de cpu e memória

--- monitor/usage_loger.py
+++ monitor/usage_loger.py
@@ -3,17 +3,16 @@ import psutil
 from datetime import datetime 
 import threading
 
-class Loger():
-    def __init__(self, infos):
-        self.exp_infos = infos
+class Loger(threading.Thread):
+    def __init__(self):
+        threading.Thread.__init__(self)
         self.log_arc = "./monitor/log_usage.csv"
         self.file_log = None
 
-    def start(self):
-        self.file_log = open(self.log_arc)
-        print("[loger] started loger")
+    def run(self):
+        self.file_log = open(self.log_arc, "a")
         sleep(1)
-        self.file_log.write(f"{psutil.cpu_percent()},{psutil.virtual_memory().percent},{self.exp_infos['n_arc']},{self.exp_infos['size_in_MB']},{self.exp_infos['arc_per_sec']},{datetime.now(tz=None)}\n")
+        self.file_log.write(f"{psutil.cpu_percent()},{psutil.virtual_memory().percent},{datetime.now(tz=None)}\n")
         self.file_log.close()
-        print("[loger] file closed")
-    
+
+ 
